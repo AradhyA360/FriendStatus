9bbbe89286f82663a612d1a568ea981a00cdd368
AradhyA360 <aradhyaaa@hotmail.com>
2024-03-26 19:25:45 +0530

    colour changed

--- src/script.js
+++ src/script.js
@@ -2,9 +2,11 @@ var buttonN = document.querySelector(".buttonN button");
 
 buttonN.addEventListener("click", function () {
   if (buttonN.innerText === "Follow") {
+    buttonN.style.backgroundColor = "green";
     buttonN.innerHTML = "Following";
     console.log("Clicked  to follow this ID");
   } else {
+    buttonN.style.backgroundColor = "black";
     buttonN.innerHTML = "Follow";
     console.log("Clicked  to unfollow this ID");
   }
